e984cbb572e2e5518b3eb37bf37fcd0acd8395b0
Aleksandr <aleksandrbartov@yandex.ru>
2018-10-27 03:26:23 +0400

    so we are going to braek my last site down

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,38 @@
-import BtnMenu from './modules/MenuAnimationBtn';
+/* import BtnMenu from './modules/MenuAnimationBtn';
 import ScrollingMenu from './modules/ScrollingForMenu';
 
-new ScrollingMenu();
-new BtnMenu();
+// new ScrollingMenu();
+new BtnMenu(); */
+
+const mover = document.getElementsByClassName('mover')[0],
+    firstPhoto = document.getElementsByClassName('photo')[0]
+
+let start, dist, counter = 0
+
+mover.onmousedown = function (e) {
+
+    start = e.pageX
+    mover.style.cursor = 'grabbing'
+
+    mover.onmousemove = function(e) {
+
+        dist = e.pageX - start
+        counter += dist
+        mover.style.left = `${counter}px`
+        firstPhoto.style.transform = `translateX(${counter/500-50}%) translateY(-50%) translateZ(${-dist}px)`
+        
+    }
+
+    mover.onmouseup = function(e) {
+        if (counter > 0) {
+            mover.style.left = '0px'
+            firstPhoto.style.transform = `translateX(-50%) translateY(-50%) translateZ(0px)`
+            counter = 0
+        }
+        
+
+      mover.onmousemove = mover.onmouseup = null;
+      
+      mover.style.cursor = 'grab'
+    }
+}
