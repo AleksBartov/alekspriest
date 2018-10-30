bffb0be40bc542007630147e4bea02285dd06c0d
Aleksandr <aleksandrbartov@yandex.ru>
2018-10-31 02:56:39 +0300

    have done with smooth-scrolling from menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,9 @@
+import $ from 'jquery';
+import smoothScroll from 'jquery-smooth-scroll';
 import HeaderFixation from './modules/HeaderFixation';
 import HiddenMenu from './modules/HiddenMenu';
 
 new HeaderFixation();
 new HiddenMenu();
+
+$('.hiddenMenu__list a').smoothScroll();
